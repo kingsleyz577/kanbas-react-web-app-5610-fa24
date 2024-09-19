1cabc3cc7c45776bcb9c4d30f7881629160f1983
kingsleyz577 <kingsleyz577@gmail.com>
2024-09-19 03:04:09 -0400

    gitlink added

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 // import './App.css';
 import Labs from "./Labs"
 import Kanbas from './Kanbas';
-import { HashRouter, Link, Route, Routes } from 'react-router-dom';
+import { HashRouter, Link, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div> 
+  return ( 
       <HashRouter>
         <div>
         <Routes>
+          <Route path="/" element={<Navigate to="/Labs" />} />
           <Route path = "/Labs/*" element={<Labs />} />
           <Route path = "/Kanbas/*" element={<Kanbas />} />
        </Routes>
        </div>
       </HashRouter>
-    </div>
   );
 }
 
